b41476c651cc953f0d09842db8c30350de04147e
Philip Hutchins <flipture@gmail.com>
2014-07-01 12:53:10 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'zabbix-agent'
+maintainer 'Philip Hutchins'
+maintainer_email 'flipture@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures Zabbix Agent'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
