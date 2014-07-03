c416dba3229ea818f2eda07060e22e400e397228
Philip Hutchins <flipture@gmail.com>
2014-07-03 09:40:16 -0400

    adding tmp_dir variable

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,6 +1,7 @@
 #package 'zabbix-agent' do
 #  action :install
 #end
+tmp_dir = node['zabbix_agent']['tmp_dir']
 
 script "download_and_install_zabbix_package" do
   command "cd #{tmp_dir} && wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb && dpkg -i zabbix-release_2.2-1+precise_all.deb"
