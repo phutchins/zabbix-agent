f7e2e4ae0ef1fceb56c6e92266c5b6d561892b0a
Philip Hutchins <flipture@gmail.com>
2014-07-03 14:16:05 -0400

    adding package install

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,9 +1,11 @@
-#package 'zabbix-agent' do
-#  action :install
-#end
 tmp_dir = node['zabbix_agent']['tmp_dir']
 
 script "download_and_install_zabbix_package" do
-  command "cd #{tmp_dir} && sudo wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb && sudo dpkg -i zabbix-release_2.2-1+precise_all.deb"
+  user "root"
+  command "cd #{tmp_dir} && sudo wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb && sudo dpkg -i zabbix-release_2.2-1+precise_all.deb && apt-get update"
   not_if "dpkg -l | grep zabbix-release"
+end
+
+package 'zabbix-agent' do
+  action :install
 end
