d26b66a0033e48ca8f82a95f5694893fe1e3f1b0
Philip Hutchins <flipture@gmail.com>
2014-07-03 10:42:58 -0400

    adding sudo to package bits

--- recipes/package.rb
+++ recipes/package.rb
@@ -4,6 +4,6 @@
 tmp_dir = node['zabbix_agent']['tmp_dir']
 
 script "download_and_install_zabbix_package" do
-  command "cd #{tmp_dir} && wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb && dpkg -i zabbix-release_2.2-1+precise_all.deb"
+  command "cd #{tmp_dir} && sudo wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb && sudo dpkg -i zabbix-release_2.2-1+precise_all.deb"
   not_if "dpkg -l | grep zabbix-release"
 end
