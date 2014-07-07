a0422dca7aefac9770230eee3b313768864819eb
Philip Hutchins <flipture@gmail.com>
2014-07-07 12:42:17 -0400

    moving to execute instead of script

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,16 +1,16 @@
 #tmp_dir = node['zabbix_agent']['tmp_dir']
 
-script "download_and_install_zabbix_package" do
+execute "download_zabbix_repo_package" do
   command "wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb -O /tmp/zabbix-release_2.2-1+precise_all.deb"
   action :run
 end
-script "install_repo_package" do
+execute "install_repo_package" do
   command "dpkg -i zabbix-release_2.2-1+precise_all.deb"
   not_if "dpkg -l | grep zabbix-release"
   action :run
 end
 
-script "apt_get_update" do
+execute "apt_get_update" do
   command "apt-get update"
   action :run
 end
